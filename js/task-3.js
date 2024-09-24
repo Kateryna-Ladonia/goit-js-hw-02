e0f6f13016ddbf34f848702ebe8fb49766a0e214
Kateryna-Ladonia <ladonyaku02@gmail.com>
2024-09-24 23:26:16 +0300

    add initial files

---
+++ js/task-3.js
@@ -0,0 +1,32 @@
+function checkForSpam(message) {
+  const messageEnd = message.toLowerCase()
+  if (messageEnd.includes("spam" && "sale")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+
+
+
+
+
+
+
+
+
+
+// function getShippingMessage(country, price, deliveryFee) {
+//   return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
+// }
+// console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
+// console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
+// console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
